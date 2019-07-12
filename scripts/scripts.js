690dad42414ba26327406a9312013f9a87f5eb42
Francisco de Assis Boldt <fboldt@gmail.com>
2019-07-12 10:47:23 -0300

    redesign content distribution

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,7 +23,7 @@ function putContent(elementId, contentFile){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById(elementId).innerHTML =
+      document.getElementById(elementId).innerHTML +=
       this.responseText;
     }
   };
