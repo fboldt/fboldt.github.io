9fb576a7fdd7c4b0bfe0407cbacdd808f3df0cdd
Francisco de Assis Boldt <fboldt@gmail.com>
2019-08-15 19:06:20 -0300

    relayout

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,3 @@
-/*
-function putContents(elementId, contentFiles){
-  divs = "";
-  for (i=0; i<contentFiles.length; i++) {
-    divs += "<div id='"+elementId+i+"'></div>";
-  }
-  document.getElementById(elementId).innerHTML = divs;
-  for (i=0; i<contentFiles.length; i++) {
-    putContent(""+elementId+i, contentFiles[i]);
-  }
-}*/
 function putContent(contentFile, classes=""){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -20,3 +9,4 @@ function putContent(contentFile, classes=""){
   xhttp.open("GET", contentFile, false);
   xhttp.send();
 }
+
